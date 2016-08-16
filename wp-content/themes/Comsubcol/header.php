3d971900c15c547cf4b702bae8bfd8dfc7cb71e6
GN4 <gn4@gaitannicholls.com>
2016-08-16 10:53:23 -0400

    Add Header for template.

---
+++ wp-content/themes/Comsubcol/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--[if IE 9]>
+<html class="ie ie9" lang="en-US">
+<![endif]-->
+<html lang="en-US">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+		<title><?php bloginfo(title); ?></title>
+		<?php wp_head(); ?>
+		<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Roboto:400,300,500,700' rel='stylesheet' type='text/css'>
+		<link href="<?php echo get_template_directory_uri(); ?>/assets/css/bootstrap.css" rel="stylesheet">
+		<link href="<?php echo get_template_directory_uri(); ?>/assets/css/animate.css" rel="stylesheet">
+		<link href="<?php echo get_template_directory_uri(); ?>/assets/css/jquery-ui.css" rel="stylesheet">
+		<link href="<?php echo get_template_directory_uri(); ?>/assets/css/simple-line-icons.css" rel="stylesheet">
+		<link href="<?php echo get_template_directory_uri(); ?>/assets/css/font-awesome.min.css" rel="stylesheet">
+		<link href="<?php echo get_template_directory_uri(); ?>/assets/css/icon-font.css" rel="stylesheet">
+		<link href="<?php echo get_template_directory_uri(); ?>/assets/css/auction.css" rel="stylesheet">
+		<link href="<?php echo get_template_directory_uri(); ?>/assets/rs-plugin/css/settings.css" rel="stylesheet">
+		<link href="<?php bloginfo(stylesheet_url); ?>" rel="stylesheet">
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+		<![endif]-->
+	</head>
