bf7f54d90d829a21923c58c414377a8daad2033c
GN4 <gn4@gaitannicholls.com>
2016-08-18 11:08:23 -0400

    Add Header section for template.

--- wp-content/themes/Comsubcol/functions.php
+++ wp-content/themes/Comsubcol/functions.php
@@ -18,3 +18,14 @@ function register_my_menus() {
 	);
 }
 add_action( 'init', 'register_my_menus' );
+
+/*
+* Function to add my custom menu item
+*/
+function my_custom_menu_item( $items, $args ) {
+	if ( $args->theme_location == 'main-menu' ) {
+		$items .= '<li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>';
+	}
+	return $items;
+}
+add_filter( 'wp_nav_menu_items', 'my_custom_menu_item', 10, 2 );
