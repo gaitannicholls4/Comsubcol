e0b5a4567295478be8c1783f20daa4c14ca58088
GN4 <gn4@gaitannicholls.com>
2016-08-18 17:59:22 -0400

    Add page for Contáctenos.

---
+++ wp-content/themes/Comsubcol/page-contactenos.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+
+Template Name: Contactenos
+
+*/
+
+?>
+<?php get_header(); ?>
+	<div id="page-heading" class="contactenos">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 text-center">
+					<h1>CONTÁCTENOS</h1>
+					<span>CON LOS MEJORES PRECIOS</span>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php get_footer(); ?>
