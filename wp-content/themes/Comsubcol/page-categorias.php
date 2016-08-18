c1acc7bf3134ddf2b0a0a8b897cd40377bdf9483
GN4 <gn4@gaitannicholls.com>
2016-08-18 16:43:07 -0400

    Add pages for Quiénes Somos and Categorías.

---
+++ wp-content/themes/Comsubcol/page-categorias.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+
+Template Name: Categorias
+
+*/
+
+?>
+<?php get_header(); ?>
+	<div id="page-heading" class="categorias">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 text-center">
+					<h1>CATEGORÍAS</h1>
+					<span>CON LOS MEJORES PRECIOS</span>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php get_footer(); ?>
---
+++ wp-content/themes/Comsubcol/page-quienes-somos.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+
+Template Name: Quienes Somos
+
+*/
+
+?>
+<?php get_header(); ?>
+	<div id="page-heading" class="quienes-somos">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 text-center">
+					<h1>ARTÍCULOS DE CALIDAD</h1>
+					<span>CON LOS MEJORES PRECIOS</span>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php get_footer(); ?>
