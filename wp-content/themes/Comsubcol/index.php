3411b2f25344765b04fd5b3ae54ed084060eb480
GN4 <gn4@gaitannicholls.com>
2016-08-22 17:42:49 -0400

    Fixed home template.

--- wp-content/themes/Comsubcol/index.php
+++ wp-content/themes/Comsubcol/index.php
@@ -1,3 +1,29 @@
 <?php get_header(); ?>
-	<?php get_template_part( 'part', 'slider' ); ?>
+	<?php
+	if ( is_home() ) {
+		get_template_part( 'part', 'slider' );
+	} else {
+	?>
+	<div id="page-heading" class="categorias">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 text-center">
+					<h1>CATEGORÍAS</h1>
+					<span>CON LOS MEJORES PRECIOS</span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	<section>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<?php the_content(); ?>
+				</div>
+			</div>
+		</div>
+	</section>
+	<?php endwhile; endif; ?>
+	<?php } ?>
 <?php get_footer(); ?>
