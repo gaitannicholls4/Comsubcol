3718d6f43bd85834adf1c827d2a0e1853a990669
GN4 <gn4@gaitannicholls.com>
2016-08-22 16:02:11 -0400

    Create function to organize CSS files.

--- wp-content/themes/Comsubcol/functions.php
+++ wp-content/themes/Comsubcol/functions.php
@@ -7,6 +7,27 @@ Functions for my template
 */
 
 /*
+* Function to add my styles files
+*/
+function my_styles_files() {
+	wp_enqueue_style( 'montserrat-css', 'https://fonts.googleapis.com/css?family=Montserrat:400,700', false );
+	wp_enqueue_style( 'roboto-css', 'https://fonts.googleapis.com/css?family=Roboto:400,300,500,700', false );
+	wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/assets/css/bootstrap.css', false );
+	wp_enqueue_style( 'animate-css', get_template_directory_uri() . '/assets/css/animate.css', false );
+	wp_enqueue_style( 'jquery-ui-css', get_template_directory_uri() . '/assets/css/jquery-ui.css', false );
+	wp_enqueue_style( 'simple-line-icons-css', get_template_directory_uri() . '/assets/css/simple-line-icons.css', false );
+	wp_enqueue_style( 'font-awesome-css', get_template_directory_uri() . '/assets/css/font-awesome.min.css', false );
+	wp_enqueue_style( 'icon-font-css', get_template_directory_uri() . '/assets/css/icon-font.css', false );
+	wp_enqueue_style( 'auction-css', get_template_directory_uri() . '/assets/css/auction.css', false );
+	wp_enqueue_style( 'rs-plugin-css', get_template_directory_uri() . '/assets/rs-plugin/css/settings.css', false );
+	if ( is_child_theme() ) {
+		wp_enqueue_style( 'parent-css', trailingslashit( get_template_directory_uri() ) . 'style.css', false );
+	}
+	wp_enqueue_style( 'theme-css', get_stylesheet_uri(), false );
+}
+add_action( 'wp_enqueue_scripts', 'my_styles_files' );
+
+/*
 * Function to register my menus
 */
 function register_my_menus() {
