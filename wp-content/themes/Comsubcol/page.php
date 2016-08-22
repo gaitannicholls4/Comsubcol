d76209b7a7d72937dc5a3ce771e70c2d6e52ab7f
GN4 <gn4@gaitannicholls.com>
2016-08-22 11:39:20 -0400

    Add general page template.

---
+++ wp-content/themes/Comsubcol/page.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	<div id="page-heading" class="general">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 text-center">
+					<h1 id="post-<?php the_ID(); ?>"><?php the_title(); ?></h1>
+					<span>CON LOS MEJORES PRECIOS</span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<section>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<?php the_content(); ?>
+				</div>
+			</div>
+		</div>
+	</section>
+	<?php endwhile; endif; ?>
+<?php get_footer(); ?>
