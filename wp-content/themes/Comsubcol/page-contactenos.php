589180fa858ab43e7217c9580d422b1ab6b9f7e9
GN4 <gn4@gaitannicholls.com>
2016-09-07 16:39:15 -0400

    Fixed page for Contáctenos.

--- wp-content/themes/Comsubcol/page-contactenos.php
+++ wp-content/themes/Comsubcol/page-contactenos.php
@@ -27,7 +27,7 @@ Template Name: Contactenos
 		</div>
 	</div>
 	<!-- Begin Google Maps -->
-		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDEH_WCmITKJzkM2YOmD4b1mPAIqLejO8w"></script>
+		<!--<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDEH_WCmITKJzkM2YOmD4b1mPAIqLejO8w"></script>
 		<script>
 			function initialize() {
 				var mapCanvas = document.getElementById('map');
@@ -40,6 +40,28 @@ Template Name: Contactenos
 				var map = new google.maps.Map(mapCanvas, mapOptions);
 			}
 			google.maps.event.addDomListener(window, 'load', initialize);
+		</script>-->
+		<script>
+			function initMap() {
+				var map = new google.maps.Map(document.getElementById('map'), {
+					center: { lat: 4.6688381, lng: -74.1512065 },
+					zoom: 16
+				});
+				var marker = new google.maps.Marker({
+					position: { lat: 4.6688381, lng: -74.1512065 },
+					map: map
+				});
+				attachSecretMessage( marker, 'Av. Centenario # 100 - 78' );
+			}
+			function attachSecretMessage( marker, secretMessage ) {
+				var infowindow = new google.maps.InfoWindow({
+					content: secretMessage
+				});
+				marker.addListener('click', function() {
+					infowindow.open( marker.get('map'), marker );
+				});
+			}
 		</script>
+		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDEH_WCmITKJzkM2YOmD4b1mPAIqLejO8w&callback=initMap&signed_in=true" async defer></script>
 	<!-- End Google Maps -->
 <?php get_footer(); ?>
