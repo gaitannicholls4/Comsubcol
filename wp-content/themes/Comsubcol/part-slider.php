558be978d18d46abde5f0dd02d75c3b962f964aa
GN4 <gn4@gaitannicholls.com>
2016-09-08 17:58:24 -0400

    Fixed slider.

--- wp-content/themes/Comsubcol/part-slider.php
+++ wp-content/themes/Comsubcol/part-slider.php
@@ -22,6 +22,16 @@
 						<?php get_template_part( 'part', 'slider-2' ); ?>
 					</div>
 				</li>
+				<li class="first-slide" data-transition="fade" data-slotamount="10" data-masterspeed="300">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/banner_3.jpg" data-fullwidthcentering="on" alt="slide">
+					<div class="tp-caption first-line lft tp-resizeme start" data-x="left" data-hoffset="0" data-y="160" data-speed="1000" data-start="200" data-easing="Power4.easeOut" data-splitin="none" data-splitout="none" data-elementdelay="0" data-endelementdelay="0">Bienvenido a</div>
+					<div class="tp-caption second-line lfb tp-resizeme start" data-x="left" data-hoffset="0" data-y="200" data-speed="1000" data-start="800" data-easing="Power4.easeOut" data-splitin="none" data-splitout="none" data-elementdelay="0" data-endelementdelay="0"><strong>Comsubcol</strong> S.A.S</div>
+					<div class="tp-caption third-line lfb tp-resizeme start" data-x="left" data-hoffset="0" data-y="280" data-speed="1000" data-start="800" data-easing="Power4.easeOut" data-splitin="none" data-splitout="none" data-elementdelay="0" data-endelementdelay="0">Visite las diferentes <em>categorías</em> de subasta</div>
+					<div class="tp-caption fourth-line lfb tp-resizeme start" data-x="left" data-hoffset="0" data-y="320" data-speed="1000" data-start="800" data-easing="Power4.easeOut" data-splitin="none" data-splitout="none" data-elementdelay="0" data-endelementdelay="0"><a href="/categorias/maquinaria-y-equipos-especiales">Maquinaria y Equipos Especiales</a> <a href="/registrarse" class="fancybox-login registrarse" data-fancybox-type="iframe">Registrese Aquí</a></div>
+					<div class="tp-caption slider-thumb sfb tp-resizeme start container hidden-xs hidden-sm" data-x="center" data-hoffset="0" data-y="400" data-speed="1000" data-start="2200" data-easing="Power4.easeOut" data-splitin="none" data-splitout="none" data-elementdelay="0" data-endelementdelay="0">
+						<?php get_template_part( 'part', 'slider-3' ); ?>
+					</div>
+				</li>
 			</ul>
 		</div>
 	</div>
